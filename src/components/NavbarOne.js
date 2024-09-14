8f9304a2fc80ce1125137742675b581cd7710a2d
Ali <ali.ouaibi@epitech.eu>
2024-09-14 11:28:59 +0200

    navbar redirection to contactpage

--- src/components/NavbarOne.js
+++ src/components/NavbarOne.js
@@ -65,7 +65,7 @@ const NavbarOne = () => {
                 </ul>
               </li>
               <li>
-                <Link to="/">CONTACT</Link>
+                <Link to="/contact">CONTACT</Link>
               </li>
             </ul>
           </div>
