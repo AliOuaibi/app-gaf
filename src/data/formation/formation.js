b6c0b3e1e6f5333ffd337673558207bde3a84ec6
Ali <ali.ouaibi@epitech.eu>
2024-09-22 12:35:12 +0200

    add react-bootstrap

--- src/data/formation/formation.js
+++ src/data/formation/formation.js
@@ -1,43 +1,341 @@
-export const formation = [{
+export const formations = [{
   id: 1
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "TITRE PRO : EXPLOITANT DE TRANSPORT ROUTIER"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
 }, {
   id: 2
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "PRÉPARATION À L'EXAMEN DE CAPACITÉ DE TRANSPORT"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
 }, {
   id: 3
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "OUVREZ VOTRE PROPRE ENTREPRISE DE TRANSPORT"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
 }, {
   id: 4
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "FIMO"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
 }, {
   id: 5
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "FCO"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
 }, {
   id: 6
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "ADR"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
+}, {
+  id: 7
+  , title: "GESTION DE CONFLIT ET MANAGEMENT EN TRANSPORT"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
+}, {
+  id: 8
+  , title: "SST ET GESTES ET POSTURES"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
+}, {
+  id: 9
+  , title: "PERFECTIONNEMENT BUREAUTIQUE"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
+}, {
+  id: 10
+  , title: "LES FONDAMENTAUX DE LA COMPTABILITÉ (CRÉATION OFFERTE)"
+  , hour: "175 heures (25 jours)"
+  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , objectif: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  } , evaluation: {
+    text: "Cette formation comprend les points suivants :"
+    , list: [
+      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "THEME 2 – Application des réglementations"
+      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
+      , "THEME 4 -Service, logistique"
+    ]
+  }
 }]
