454230fa36509bf6f9eee73386af533d5d734fa9
Ali <ali.ouaibi@epitech.eu>
2024-09-22 12:36:37 +0200

    WIP

--- src/data/formation/formation.js
+++ src/data/formation/formation.js
@@ -1,17 +1,30 @@
-export const formation = [{
+export const formations = [{
   id: 1
-  , title: "Poids lourds"
-  , hour: "20jours (140heurs)"
-  , startDate: "10/09/2024"
-  , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , title: "PREPARATION A L’EXAMEN D’ATTESTATION DE CAPACITE DE COMMISSIONNAIRE DE TRANSPORT"
+  , hour: "175 heures (25 jours)"
+  , startDate: "À la demande"
+  , price: "2 795€"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , detail: "Appliquer le droit au transport.\n" +
+    "Appliquer le droit social au transport.\n" +
+    "Intégrer l’économie des transports et activités du Commissionnaire.\n" +
+    "Analyser la gestion commerciale et financière de l’entreprise.\n" +
+    "S’approprier la terminologie professionnelle."
+  , objectif: "Appliquer le droit au transport.\n" +
+    "Appliquer le droit social au transport.\n" +
+    "Intégrer l’économie des transports et activités du Commissionnaire.\n" +
+    "Analyser la gestion commerciale et financière de l’entreprise.\n" +
+    "S’approprier la terminologie professionnelle."
+  , evaluation: "En cours de formation : examens blancs\n" +
+    "A l’issue de la formation : Présentation des candidats à l’examen national organisé par le\n" +
+    "Ministère de l’Ecologie, du Développement durable et de l’Energie."
 }, {
   id: 2
   , title: "Poids lourds"
   , hour: "20jours (140heurs)"
   , startDate: "10/09/2024"
   , price: "2 500€"
-  , bannerImage: "assets/img/formation/GAF-03.png"
+  , bannerImage: "assets/img/formation/commissionnaire.webp"
 }, {
   id: 3
   , title: "Poids lourds"
