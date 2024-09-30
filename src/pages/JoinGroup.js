f1f3fba38f31ef4b09f06d69cd55dc32ea822777
Ali <ali.ouaibi@epitech.eu>
2024-09-30 12:41:10 +0200

    create page join group

---
+++ src/pages/JoinGroup.js
@@ -0,0 +1,26 @@
+import React, {Fragment, Suspense} from "react";
+import Preloader from "../elements/Preloader";
+import Breadcrumb from "../components/Breadcrumb";
+import JoinGroupContent from "../components/JoinGroup/JoinGroupContent";
+
+const FooterBottomOne = React.lazy(() =>
+  import("../components/FooterBottomOne")
+);
+const NavbarOne = React.lazy(() => import("../components/NavbarOne"));
+
+const JoinGroup = () => {
+  return (
+    <>
+      <Fragment>
+        <Suspense fallback={<Preloader/>}>
+          <NavbarOne/>
+          <Breadcrumb title={"REJOINDRE LE GROUPE"} banner={"formation-bg.png"} video/>
+          <JoinGroupContent />
+          <FooterBottomOne />
+        </Suspense>
+      </Fragment>
+    </>
+  );
+};
+
+export default JoinGroup;
