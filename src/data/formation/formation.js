ecd28b57f7fe27be062ac7d55e67b5db689c5313
Ali <ali.ouaibi@epitech.eu>
2024-10-09 18:45:20 +0200

    update page and data formation

--- src/data/formation/formation.js
+++ src/data/formation/formation.js
@@ -1,12 +1,12 @@
 export const formations = [{
   id: 1
-  , title: "TITRE PRO : EXPLOITANT DE TRANSPORT ROUTIER"
+  , title: "OUVREZ VOTRE PROPRE ENTREPRISE DE TRANSPORT"
   , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , location: "RIS-ORANGIS (91)"
   , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
   , startDate: "À la demande"
   , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , bannerImage: "assets/img/formation/7.png"
   , detail: {
     text: "Cette formation comprend les points suivants :"
     , list: [
@@ -36,41 +36,37 @@ export const formations = [{
   id: 2
   , title: "PRÉPARATION À L'EXAMEN DE CAPACITÉ DE TRANSPORT"
   , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , location: "RIS-ORANGIS (91)"
   , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
   , startDate: "À la demande"
   , price: "2 795€"
   , bannerImage: "assets/img/formation/commissionnaire.webp"
   , detail: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Appliquer le droit au transport."
+      , "Appliquer le droit social au transport."
+      , "Intégrer l’économie des transports et activités du Commissionnaire."
+      , "Analyser la gestion commerciale et financière de l’entreprise."
+      , "S’approprier la terminologie professionnelle."
     ]
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: null
+    , list: null
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "En cours de formation : examens blancs\n" +
+      "A l’issue de la formation : Présentation des candidats à l’examen national organisé par le\n" +
+      "Ministère de l’Ecologie, du Développement durable et de l’Energie, le 1er mercredi du\n" +
+      "mois d’octobre de l’année en cours."
     ]
   }
 }, {
   id: 3
-  , title: "OUVREZ VOTRE PROPRE ENTREPRISE DE TRANSPORT"
+  , title: "TITRE PRO : EXPLOITANT DE TRANSPORT ROUTIER"
   , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , location: "RIS-ORANGIS (91)"
   , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
   , startDate: "À la demande"
   , price: "2 795€"
@@ -103,12 +99,12 @@ export const formations = [{
 }, {
   id: 4
   , title: "FIMO"
-  , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , hour: "140 heures (20 jours)"
+  , location: "RIS-ORANGIS (91)"
   , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
   , startDate: "À la demande"
-  , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , price: "2 050€"
+  , bannerImage: "assets/img/formation/fimo001.webp"
   , detail: {
     text: "Cette formation comprend les points suivants :"
     , list: [
@@ -118,197 +114,173 @@ export const formations = [{
       , "THEME 4 -Service, logistique"
     ]
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Etre capable d’exercer le métier de conducteur routier marchandises " +
+      "dans le respect de la sécurité et de la réglementation professionnelle en assurant un service de qualité."
+      , "Le programme de la formation prend en compte les préconisations prévues par l’avenant n° 5 à " +
+      "l’accord du 26/08/1999, des Conventions Collectives Nationale du Bâtiment et des Travaux Publics (n° 3005, 3193, 3258)."
     ]
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Pour la partie théorique : QCM de 60 questions (recevabilité à 36 bonnes réponses)"
+      , "Pour la partie pratique : Contrôle continu"
+      , "L’évaluation finale est réalisée par un Formateur Evaluateur"
+      , "Formation validée par la délivrance de la carte de qualification de conducteur"
     ]
   }
 }, {
   id: 5
   , title: "FCO"
-  , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
-  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , hour: "35 heures (5 jours consécutifs)"
+  , location: "RIS-ORANGIS (91)"
+  , peopleValid: "Tout conducteur, salarié ou non, principal ou occasionnel, du transport de marchandises. " +
+    "Être titulaire d’un permis C, C1, C1E ou CE en cours de validité Être titulaire d’une " +
+    "FIMO marchandises ou attestation équivalent FIMO"
   , startDate: "À la demande"
-  , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , price: "617€"
+  , bannerImage: "assets/img/formation/Sans-titre-2.webp"
   , detail: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Bilan des connaissances"
+      , "Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
+      , "Application des règlementations"
+      , "Santé, sécurité routière et sécurité environnementale"
+      , "Service, logistique"
+      , "Evaluation des acquis et synthèse de stage"
     ]
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Se perfectionner à une conduite axée sur les règles de sécurité,"
+      , "Actualiser ses connaissances en matière de réglementation du transport ainsi que de santé, " +
+      "sécurité routière, sécurité environnementale, service et logistique"
     ]
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Un bilan de connaissance aura lieu en début et fin de formation"
     ]
   }
 }, {
   id: 6
-  , title: "ADR"
-  , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
-  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , title: "ADR 8.2 – INITIALE ET RECYCLAGE"
+  , hour: "1 à 5  jours (selon le type d'ADR)"
+  , location: "RIS-ORANGIS (91)"
+  , peopleValid: "Tout public en possession d'un permis B, C et CE"
   , startDate: "À la demande"
-  , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , price: "À partir de 300€"
+  , bannerImage: "assets/img/formation/8.png"
   , detail: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "8.2 IC – Initial Citerne, 4 jours"
+      , "8.2 IPP – Initial Produits Pétroliers, 2 jours"
+      , "8.2 IB – Initial Base, 3 jours"
+      , "8.2 RB – Recyclage Base"
+      , "8.2 RC – Recyclage Citerne"
+      , "8.2 RPP – Recyclage Produits Pétroliers"
     ]
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Acquérir les connaissances nécessaires au transport routier de marchandises dangereuses, conformément à l’ADR en vigueur et Arrêté TMD"
+      , "Obtenir le certificat ADR Base (sauf 1 et 7)"
+      , "Recycler les connaissances nécessaire au transport routier de marchandises dangereuses, conformément à l’ADR en vigueur et Arrêté TM"
     ]
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Feuille de présence"
+      , "Questions orales et écrites"
+      , "Travaux pratiques"
+      , "Questionnaire d’examen"
     ]
   }
 }, {
   id: 7
   , title: "GESTION DE CONFLIT ET MANAGEMENT EN TRANSPORT"
-  , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
-  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , hour: "14 heures (2 jours)"
+  , location: "RIS-ORANGIS (91)"
+  , peopleValid: "Public occupant un poste à responsabilité avec une équipe à manager"
   , startDate: "À la demande"
-  , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , price: "À partir de 1 000 € pour un groupe maximum de 12 personnes"
+  , bannerImage: "assets/img/formation/elea001.webp"
   , detail: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Inévitable dans la vie d’une équipe, les différends sont aussi l’occasion de repartir du bon pied.\n" +
+      "A condition de s’y être préparé. Ce programme est fait pour déminer le terrain, rapprocher les points de vue… et hausser le ton quand c’est nécessaire."
+    , list: null
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
+    text: null
     , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
+      "Plan de batailles : Eviter les voies sans issue"
+      , "Déminage : Appliquer les tactiques antichocs"
+      , "Conciliation : Négocier un accord GAGNANT-GAGNANT"
+      , "Manière forte : Garder son sang-froid"
+      , "Situation à Risque : Déjouer tous les pièges"
+      , "Pendant la tempête : Limiter la casse en temps de crise"
+      , "Après la bataille : Repartir sur de nouvelles Bases"
     ]
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Evaluation collective en interaction"
+    , list: null
   }
 }, {
   id: 8
   , title: "SST ET GESTES ET POSTURES"
-  , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
-  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , hour: "1 jours (7h)"
+  , location: "RIS-ORANGIS (91)"
+  , peopleValid: "Tout public"
   , startDate: "À la demande"
-  , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , price: "210€"
+  , bannerImage: "assets/img/formation/gp001.webp"
   , detail: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Notre vie professionnelle est faite de gestes et de postures qui reviennent plusieurs fois " +
+      "par jour, qui sollicitent plus ou moins l’organisme, qui impactent plus ou moins durablement la santé.\n" +
+      "Le but est d’avoir les bons réflexes devant une situation de portage de charges et être capable de protéger son râchis lombaire."
+    , list: null
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Le but est d’avoir les bons réflexes devant une situation de portage de charges et être capable de protéger son râchis lombaire."
+    , list: null
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: null
+    , list: null
   }
 }, {
   id: 9
   , title: "PERFECTIONNEMENT BUREAUTIQUE"
-  , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
-  , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
+  , hour: "12 jours – (84 h)"
+  , location: "RIS-ORANGIS (91)"
+  , peopleValid: "Toute personne, salarié ou non."
   , startDate: "À la demande"
-  , price: "2 795€"
-  , bannerImage: "assets/img/formation/commissionnaire.webp"
+  , price: "3 325€"
+  , bannerImage: "assets/img/formation/off001-300x127.webp"
   , detail: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Acquérir les notions de base à l’utilisation de Microsoft EXCEL et se perfectionner\n" +
+      "Acquérir les notions de base à l’utilisation de Microsoft OUTLOOK et se perfectionner\n" +
+      "Acquérir les notions de base à l’utilisation de Microsoft POWER POINT et se perfectionner\n" +
+      "Acquérir les notions de base à l’utilisation de Microsoft WORD et se perfectionner\n" +
+      "Obtenir les certifications niveau « Opérationnel » ou « Avancé »"
+    , list: null
   } , objectif: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Acquérir les notions de base à l’utilisation de Microsoft EXCEL et se perfectionner\n" +
+      "Acquérir les notions de base à l’utilisation de Microsoft OUTLOOK et se perfectionner\n" +
+      "Acquérir les notions de base à l’utilisation de Microsoft POWER POINT et se perfectionner\n" +
+      "Acquérir les notions de base à l’utilisation de Microsoft WORD et se perfectionner\n" +
+      "Obtenir les certifications niveau « Opérationnel » ou « Avancé »"
+    , list: null
   } , evaluation: {
-    text: "Cette formation comprend les points suivants :"
-    , list: [
-      "THEME 1 – Perfectionnement à la conduite rationnelle axée sur les règles de sécurité"
-      , "THEME 2 – Application des réglementations"
-      , "THEME 3 – Santé, sécurité routière et sécurité environnementale"
-      , "THEME 4 -Service, logistique"
-    ]
+    text: "Un bilan de connaissance informatique en début et en fin de formation.\n" +
+      "Ainsi que des exercices test au cours de la formation."
+    , list: null
   }
 }, {
   id: 10
   , title: "LES FONDAMENTAUX DE LA COMPTABILITÉ (CRÉATION OFFERTE)"
   , hour: "175 heures (25 jours)"
-  , location: "Lieusaint (77), Villeneuve-Saint-Georges (94)"
+  , location: "RIS-ORANGIS (91)"
   , peopleValid: "Tout conducteur titulaire d’un permis C, C1, C1E ou CE en cours de validité"
   , startDate: "À la demande"
   , price: "2 795€"
